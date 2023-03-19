2e37a9413e64443d0668ffbca69d8ce3d8c14e80
Xstasy <x@strum.bar>
2023-03-19 14:51:04 +0100

    Removed dependency Bounce2 and replaced with rewritten Bounce3 for microsecond resolution on inputs

--- src/guitar.cpp
+++ src/guitar.cpp
@@ -5,7 +5,7 @@
         const TickType_t xDelay = BATTERY_HZ / portTICK_PERIOD_MS;
         for( ;; ) {
             bTot = bTot - bSamples[bIndex];
-            bSamples[bIndex]  = adc1_get_raw(bc);
+            bSamples[bIndex]  = analogRead(bp);
             bTot = bTot + bSamples[bIndex];
             bIndex = bIndex + 1;
             if (bIndex >= BATTERY_SAMPLES)  bIndex = 0;
@@ -14,7 +14,7 @@
             if(bIndex == 0) {
                 if(bAvg < bMin) bAvg = bMin;
                 int lvl = map(bAvg, bMin, 4095, 0, 100);
-                pad.setBatteryLevel(lvl);
+                //pad.setBatteryLevel(lvl);
                 pad.setSlider1(map(bAvg, bMin, 4095, 0, 32767));
                 #if defined(DEBUG_MODE) || defined(DEBUG_BATTERY)
                     Serial.printf("Battery: %d - Raw: %d\n", lvl, bSamples[bIndex]);
@@ -31,8 +31,10 @@
         bool ft = true;
         for( ;; ) { 
             wTot = wTot - wSamples[wIndex];
-            wSamples[wIndex] = adc1_get_raw(wc);
-            
+            wSamples[wIndex] = analogRead(wp);
+            #if defined(DEBUG_MODE) || defined(DEBUG_WHAMMY)
+                Serial.printf("%d, ", wSamples[wIndex]);
+            #endif
             if(wSamples[wIndex] > wMax) wMax =  wSamples[wIndex];
             if(wSamples[wIndex] < wMin) wMin =  wSamples[wIndex];
             
@@ -60,50 +62,36 @@
     };
 #endif
 
-bool Button(int b, bool s = true) {
-    bool db = false;
+bool Button(int b, bool pressed = true) {
+    if(pressed) {
+        if(b >= 8) pad.setHat1(b == 8 ? HAT_UP : HAT_DOWN);
+        else pad.press(b+1);
+    } else if(b >= 8) 
+        pad.setHat1(HAT_CENTERED);
+    else
+        pad.release(b+1);
+
     #if defined(DEBUG_MODE) || defined(DEBUG_BUTTONS)
-        db = true;
+        if(b >= Strum::UP) Serial.printf("Strumbar: %s\n", pressed ? (b == 8 ? "Up" : "Down") : "Centered");
+        else Serial.printf("%s: %s\n", bN[b], pressed ? (b < 8 ? "Pressed" : "Tilted") : (b < 8 ? "Released" : "Normal"));
     #endif
-    if(b < 8) {
-        if(s) pad.press(b+1);
-        else pad.release(b+1);
-        if(db) Serial.printf("%s: %s\n", bN[b], s ? (b < 8 ? "Pressed" : "Tilted") : (b < 8 ? "Released" : "Normal"));
-    } else {
-        if(s) {
-            pad.setHat1(b == 8 ? HAT_UP : HAT_DOWN);
-            if(db) Serial.printf("Strumbar: %s\n", b == 8 ? "Up" : "Down");
-        } else {
-            pad.setHat1(HAT_CENTERED);
-            if(db) Serial.println("Strumbar: Centred");
-        } 
-    }
     return true;
 }
 
 void setup() {
-    if(ser) {
+    #if defined(DEBUG_MODE) || defined(DEBUG_BATTERY) || defined(DEBUG_BUTTONS) || defined(DEBUG_WHAMMY) || defined(DEBUG_LOOP_TIME)
         Serial.begin(115200);
         while(!Serial) { delay(50); };
         delay(1000);
-        Serial.printf("\r\n\r\nBLEUltraGuitar");
-    }
-
-    if(wp > 0 || bp > 0) {
-        adc1_config_width(ADC_WIDTH_12Bit);
-        #if defined(WHAMMY_PIN) && WHAMMY_PIN > 0
-            adc1_config_channel_atten(wc, ADC_ATTEN_11db);
-        #endif
-        #if defined(BATTERY_PIN) && BATTERY_PIN > 0
-            adc1_config_channel_atten(bc, ADC_ATTEN_11db);
-        #endif
-    }
+        Serial.println("\r\n\r\n");
+        Serial.println("BLEUltraGuitar");
+    #endif
 
     for(int i = 0; i < 10; i++) {
         btns[i].attach ( (int)bpins[i], INPUT_PULLUP);
-        if(i < 8)       btns[i].interval(FRET_BOUNCE_MS);
-        else if(i == 8) btns[i].interval(STRUM_UP_BOUNCE_MS);
-        else if(i == 9) btns[i].interval(STRUM_DOWN_BOUNCE_MS);
+        if(i < 8)       btns[i].interval(FRET_BOUNCE_uS);
+        else if(i == 8) btns[i].interval(STRUM_UP_BOUNCE_uS);
+        else if(i == 9) btns[i].interval(STRUM_DOWN_BOUNCE_uS);
         #if defined(DEBUG_MODE) || defined(DEBUG_BUTTONS)
             Serial.printf("%s: configured using pin %d\n", bN[i], bpins[i]);
         #endif
@@ -111,16 +99,15 @@ void setup() {
 
     cfg.setAutoReport(0);
     cfg.setWhichAxes(0, 0, 0, 0, 0, 0, wp != 0 ? 1 : 0, bp != 0 ? 1 : 0);
-    cfg.setButtonCount(8);
+    cfg.setButtonCount(tp > 0 ? 8 : 7);
     cfg.setHatSwitchCount(1);
-    cfg.setVid(0x0420);
-    cfg.setPid(0x6969);
+    cfg.setVid(0x1430);
+    cfg.setPid(0x4748);
     pad.begin(&cfg);
 
     #if defined(WHAMMY_PIN) && WHAMMY_PIN > 0
         xTaskCreate(&wTask, "wTask", 10000, NULL, 5,  NULL);
     #endif
-    
     
     #if defined(BATTERY_PIN) && BATTERY_PIN > 0
         xTaskCreate(&bTask, "bTask", 10000, NULL, 10, NULL);
@@ -129,15 +116,13 @@ void setup() {
             esp_err_t error = esp_sleep_enable_ext0_wakeup(static_cast<gpio_num_t>(bpins[i]), 0);
             switch(error) {
                 case ESP_OK:
-                    #if defined(DEBUG_MODE) || defined(DEBUG_SLEEP)
-                        Serial.printf("Sleep wakeup enabled on %s (pin: %d)\n", bN[i], bpins[i]);
-                    #endif
+                    if(Serial) Serial.printf("Sleep wakeup enabled on %s (pin: %d)\n", bN[i], bpins[i]);
                     break;
                 case ESP_ERR_INVALID_ARG:
-                    if(ser) Serial.printf("Sleep wakeup failed on pin: %d - GPIO is not an RTC GPIO\n", bpins[i]);
+                    if(Serial) Serial.printf("Sleep wakeup failed on pin: %d - GPIO is not an RTC GPIO\n", bpins[i]);
                     break;
                 case ESP_ERR_INVALID_STATE:
-                    if(ser) Serial.printf("Sleep wakeup failed on pin: %d - Wakeup triggers conflict\n", bpins[i]);
+                    if(Serial) Serial.printf("Sleep wakeup failed on pin: %d - Wakeup triggers conflict\n", bpins[i]);
                     break;
                 default:
                     break;
@@ -147,8 +132,9 @@ void setup() {
 }
 
 void loop() {
-    ticks++;
-    long s = micros();
+    #ifdef DEBUG_LOOP_TIME
+        long s = micros();
+    #endif
 
     for(int i = 0; i < 10; i++) {
         btns[i].update();
@@ -156,18 +142,19 @@ void loop() {
         else if(btns[i].rose()) update = Button(i, 0);
     }
     
-    if(ticks >= 20000) {
-        ticks = 0;
-        if((millis() - lastHit && bp > 0) > 120000)
-            esp_deep_sleep_start();
-    }
+    #if defined(BATTERY_PIN) && BATTERY_PIN > 0
+        ticks++;
+        if(ticks >= 20000) {
+            ticks = 0;
+            if((millis() - lastHit) > 120000)
+                esp_deep_sleep_start();
+        }
+    #endif
     
     if(update) {
-        lastHit = millis();   
+        if(bp > 0) lastHit = millis();   
         pad.sendReport();
     }
-    
-    delayMicroseconds(180);
 
     #ifdef DEBUG_LOOP_TIME
         Serial.printf("%d, ", (micros() - s));
--- src/guitar.h
+++ src/guitar.h
@@ -1,14 +1,8 @@
 #include <Arduino.h>
-#include <Bounce2.h>
+#include <Bounce3.h>
 #include <BleGamepad.h>
 #include "esp_adc_cal.h"
 #include <config.h>
-
-#if defined(DEBUG_MODE) || defined(DEBUG_BATTERY) || defined(DEBUG_BUTTONS) || defined(DEBUG_LOOP_TIME)
-    bool ser = true;
-#else
-    bool ser = false;
-#endif
 
 const char* bN[10] = { 
     (char*)"Green",
@@ -25,20 +19,24 @@ const char* bN[10] = {
 };
 
 Bounce btns[10] = { Bounce(), Bounce(), Bounce(), Bounce(), Bounce(), Bounce(), Bounce(), Bounce(), Bounce(), Bounce() };
-int bpins[10] = { FRET_GREEN, FRET_RED, FRET_YELLOW, FRET_BLUE, FRET_ORANGE, SELECT, START, TILT, STRUM_UP, STRUM_DOWN };
+int bpins[10] = { FRET_GREEN, FRET_RED, FRET_YELLOW, FRET_BLUE, FRET_ORANGE, BUTTON_SELECT, BUTTON_START, TILT_PIN, STRUM_UP, STRUM_DOWN };
 BleGamepad pad = BleGamepad(DEVICE_NAME, "Strum.bar", 100);
 BleGamepadConfiguration cfg;
-
 bool update = false;
 int ticks = 0;
 int lastHit = millis();
+
+#if defined(TILT_PIN) && TILT_PIN > 0
+    int tp = TILT_PIN;
+#else
+    int tp = 0;
+#endif
 
 #if defined(WHAMMY_PIN) && WHAMMY_PIN > 0
     int wp = WHAMMY_PIN;
     int wMin = 32767;
     int wSamples[WHAMMY_SAMPLES];
     int wIndex, wAvg, wTot, wMax, wLast = 0;
-    adc1_channel_t wc = (wp == 34 ? ADC1_CHANNEL_6 : (wp == 35  ? ADC1_CHANNEL_7 : (wp == 36 ? ADC1_CHANNEL_0 : ADC1_CHANNEL_3)));
 #else 
     int wp = 0;
 #endif
@@ -48,7 +46,6 @@ int lastHit = millis();
     int bIndex, bAvg, bTot = 0;
     int bSamples[BATTERY_SAMPLES];
     int bMin = 2500;
-    adc1_channel_t bc = (bp == 34 ? ADC1_CHANNEL_6 : (bp == 35  ? ADC1_CHANNEL_7 : (bp == 36 ? ADC1_CHANNEL_0 : ADC1_CHANNEL_3)));
 #else
     int bp = 0;
 #endif
